67e968dcc4e75f8256f9bc39011afe5fa9db1923
ser888000 <cto8@ya.ru>
2022-06-04 17:27:38 +0300

    add

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoAlertPresentException 
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
 from .locators import BasePageLocators
 import math
 
